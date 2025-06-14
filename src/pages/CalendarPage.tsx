1bee5cf587291ef08ad599fa6b06a7248414301d
Jewa-bro <wlsdyd103@gmail.com>
2025-06-14 23:40:12 +0900

    feat: 캘린더 페이지에 할 일 상세 패널 복원

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -9,13 +9,24 @@ const CalendarPage: React.FC = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="p-0 sm:p-4 md:p-6 h-full flex flex-col"
+      className="h-full flex flex-col"
     >
-      <div className="flex-grow lg:flex lg:gap-6 h-full">
-        <div className="h-full flex-grow lg:flex-grow-0 lg:w-2/3">
+      {/* --- 데스크탑 레이아웃 (lg 사이즈 이상) --- */}
+      <div className="hidden lg:flex lg:gap-6 h-full p-4 md:p-6">
+        <div className="lg:w-2/3 h-full">
           <Calendar />
         </div>
-        <div className="hidden lg:block lg:w-1/3 h-full">
+        <div className="lg:w-1/3 h-full">
+          <DateDetailPanel />
+        </div>
+      </div>
+
+      {/* --- 모바일 레이아웃 (lg 사이즈 미만) --- */}
+      <div className="lg:hidden flex flex-col h-full">
+        <div className="h-[60%] border-b border-gray-200">
+          <Calendar />
+        </div>
+        <div className="h-[40%]">
           <DateDetailPanel />
         </div>
       </div>
